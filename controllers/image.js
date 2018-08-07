bb309755ca59df26cb7c112b14d0d3486038f4fb
Eloisa Morais <eloisa.smorais@gmail.com>
2018-08-07 02:00:42 -0300

    descubra

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'YOUR_API-_KEY_HERE'
+    apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
